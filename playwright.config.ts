e161eb1afe9fcf1de88712bd6c353928f489d4d6
Sarath Balla <sarathballa143@gmail.com>
2026-03-20 12:48:41 +0530

    #AddingFullyParallel

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,13 +24,13 @@ export default defineConfig({
   },
   testDir: './tests/ui-tests',
   /* Run tests in files in parallel */
-  fullyParallel: false,
+  fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 3 : 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [['html', { open: 'never' }],['junit', {outputFile: 'test-results/junit-report.xml'}]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
